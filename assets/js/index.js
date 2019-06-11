b3877d6b5ecca265b47c852cc864f29a388e436a
NguyenSonArsenal <vanson297.nguyen@gmail.com>
2019-06-12 00:12:16 +0700

    +init

---
+++ assets/js/index.js
@@ -0,0 +1,111 @@
+// executes when complete page is fully loaded, including all frames, objects and images
+$(window).on('load', function () {
+    //setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+
+function removeLoader() {
+    $('#loading').fadeOut(500, function () {
+        $(this).hide();
+        $('#wrapper').fadeIn(500, function () {
+            $('#wrapper').show();
+        });
+    });
+}
+
+// executes when HTML-Document is loaded and DOM is ready
+$(document).ready(function () {
+    // Cache the Window object
+    $window = $(window);
+    $nav = $('nav');
+
+    $('.navbar-toggler').click(function () {
+        var toggle = $(this).attr('aria-expanded');
+        var img = $(this).children('img');
+        if (toggle == 'false') {
+            img.attr('src', './assets/images/close.png');
+            $(this).parents('nav').addClass('open');
+        }else {
+            img.attr('src', './assets/images/menu.png');
+            $(this).parents('nav').removeClass('open');
+        }
+    });
+
+    // Use the cached window object here
+    $window.scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        toggleIconBackToTop(scrollTop);
+        toggleStyleNavbar(scrollTop, $nav);
+    });
+
+    $('#icon-back-to-top').click(function () {
+        $('body, html').animate({scrollTop: 0}, 600);
+    });
+
+    if (isMobile()) {
+        $('#service .description').addClass('text-center');
+    } else {
+        $('#service .description').addClass('text-left');
+    }
+
+    // scroll to tab content when click menu
+    $li = $('.nav-item');
+    $navItem = $('.nav-item__link');
+    $li.click(function (e) {
+        e.preventDefault();
+        $navItem.removeClass('active');
+        console.log($(this).data('scroll'));
+        $('.nav-item__link_' + $(this).data('scroll')).addClass('active');
+        goToByScroll($(this).data('scroll'));
+    });
+
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        autoHeight: true, //enable auto height
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        parallax: true,
+        speed: 600,
+        autoplay: 5000,
+        breakpoints: {
+            568: {
+                slidesPerView: 1,
+                slidesPerGroup: 1
+            },
+            767: {
+                slidesPerView: 2,
+                slidesPerGroup: 2
+            },
+
+        }
+    })
+
+});
+
+var scrollPointbackTop = 100// px
+var scrollPointNavbar = 150; // px
+function toggleIconBackToTop(scrollTop) {
+    if (scrollTop >= scrollPointbackTop) {
+        $('#icon-back-to-top').addClass('show');
+    } else {
+        $('#icon-back-to-top').removeClass('show');
+    }
+}
+
+function toggleStyleNavbar(scrollTop, navbar) {
+    if (scrollTop >= scrollPointNavbar) {
+        navbar.addClass('fixed-ontop');
+    } else {
+        if (navbar.hasClass('fixed-ontop')) {
+            navbar.removeClass('fixed-ontop');
+        }
+    }
+}
+
+// Scroll menu
+function goToByScroll(id) {
+    $('html,body').animate({scrollTop: ($("#" + id).offset().top-80)}, 'slow');
+}
---
+++ index.php
@@ -0,0 +1,156 @@
+<html lang="vi" class="">
+<head lang="vi">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="./assets/images/favicon.ico" type="image/x-icon" rel="icon">
+    <link href="/bower_components/bootstrap4/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="/bower_components/animate/animate.min.css" rel="stylesheet" type="text/css">
+    <link href="/assets/css/common.css" rel="stylesheet" type="text/css">
+    <link href="/assets/css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700&amp;subset=vietnamese&&w=2.2.7.22008864"/>
+
+    <script src="/bower_components/jquery/jquery-3.3.1.min.js"></script>
+    <script src="/bower_components/bootstrap4/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div id="loading" style="display: none">
+    <img id="loading-image" src="./assets/images/loading.gif" alt="Loading...">
+</div>
+<div id="wrapper">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="./assets/images/logo.svg" alt="Logo">
+                <span class="company-name">Paraline</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-expanded="false">
+                <img src="./assets/images/menu.png" alt="image name">
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav navbar-right ml-auto">
+                    <li class="nav-item" data-scroll="home">
+                        <a class="nav-item__link nav-item__link_home color-grey active" href="#">
+                            <span>Lên đỉnh</span>
+                        </a>
+                    </li>
+                    <li class="nav-item" data-scroll="service">
+                        <a class="nav-item__link nav-item__link_service color-grey" href="#">
+                            <span>Chém gió</span>
+                        </a>
+                    </li>
+                    <li class="nav-item" data-scroll="about">
+                        <a class="nav-item__link nav-item__link_about color-grey" href="#">
+                            <span>PR bản thân</span>
+                        </a>
+                    </li>
+                    <li class="nav-item" data-scroll="partner">
+                        <a class="nav-item__link nav-item__link_partner color-grey" href="#">
+                            <span>Liên hệ</span>
+                        </a>
+                    </li>
+                    <li class="nav-item lang" data-scroll="tab-04">
+                        <div class="row text-center">
+                            <div class="col">
+                                <a href="#" class="lang--flag">
+                                    <img src="./assets/images/vn.svg" alt="">
+                                </a>
+                            </div>
+                            <div class="col">
+                                <a href="#" class="lang--flag">
+                                    <img src="./assets/images/gb.svg" alt="">
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Description: Top page -->
+    <div class="rowx" id="home">
+        <div class="container">
+            <div id="top" class="text-center">
+                <h1><span class="color-green">Nguyen Son Arsenal</span></h1>
+                <h3>
+                    <span class="sp-break-line">Tự nhận mình là người khá hài hước. Cảm ơn bạn đã ghé thăm tường nhà Sơn</span>
+                </h3>
+                <p class="color-grey fadeInDown animated sort-description">
+                    Bạn chỉ cần siêng thôi, bạn càng dễ đạt được. <br>
+                    Còn bạn lười, chắc chắn bạn sẽ mãi như thời điểm vô tình đọc được những dòng này.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Content post -->
+    <div class="rowx" id="">
+        <div class="container">
+            <div class="link-inline-block bounceIn animated">
+                <h2><a href="javascript;void(0)" class="">Chém gió</a></h2>
+            </div>
+        </div>
+    </div>
+
+    <!-- Content: My profile -->
+    <div class="rowx" id="">
+        <div class="container">
+            <div class="link-inline-block bounceIn animated">
+                <h2><a href="javascript;void(0)" class="">PR bản thân</a></h2>
+            </div>
+            <h3>Thông tin cá nhân</h3>
+            <p>Họ tên: Nguyễn Văn Sơn </p>
+            <p>Email: sonnv.paraline@gmail.com</p>
+            <p>Địa chỉ làm việc: Tầng 15, Tòa nhà Viwaseen, Đường Tố Hữu, Quận Nam Từ Liêm, TP. Hà Nội, Việt Nam</p>
+            <p>Trường đại học: Đại Học Bách Khoa Hà Nội</p>
+            <p>Trường cấp 3: THPT Thiệu Hóa</p>
+            <p>Trường cấp 2: THCS Thiệu Phúc</p>
+            <p>Link facebook: <a href="https://www.facebook.com/nguyen.son.9615">Chạm là tới</a></p>
+            <h3>Kinh nghiệm làm việc</h3>
+            <p>Alt Plus Việt Nam: 6/2017 tới 8/2017</p>
+            <p>VNP: 12/2018 tới 5/2019</p>
+            <p>ParaLine VietNam Co., Ltd.: 6/2019 tới nay</p>
+            <h3>Kỹ năng</h3>
+            <p>Frontend: HTML5, CSS3, Javascript, FW7</p>
+            <p>Backend: PHP (Laravel 5x)</p>
+        </div>
+    </div>
+
+    <!-- Content: Contact -->
+    <div class="rowx contact color-grey" id="contact">
+        <div class="container">
+            <div class="link-inline-block bounceIn animated">
+                <h2><a href="javascript;void(0)" class="">Liên hệ</a></h2>
+            </div>
+            <div class="contact__list">
+                <ul>
+                    <li><a href="javascript"><i class="fa fa-facebook" aria-hidden="true"></i>Facebook</a></li>
+                    <li><a href="javascript"><i class="fa fa-google" aria-hidden="true"></i>sonnv.paraline@gmail.com</a></li>
+                    <li><a href="javascript"><i class="fa fa-skype" aria-hidden="true"></i>nguyensonbk2</a></li>
+                    <li><a href="javascript"><i class="fa fa-soundcloud" aria-hidden="true"></i>SoundCloud</a></li>
+                    <li><a href="javascript"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-center">
+        <div class="color--grey">© 2019 NguyenSonArsenal <img src="./assets/images/heart.svg" alt="image name"> Tại Hà
+            Nội
+        </div>
+        <div class="footer__back-to-top">
+            <img id="icon-back-to-top" src="./assets/images/arrow-up-icon.png" alt="Back to top" width="40px">
+        </div>
+    </footer>
+</div>
+</body>
+<script src="./assets/js/common.js"></script>
+<script src="./assets/js/index.js"></script>
+</html>
+
