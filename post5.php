aec7dd02c249a514bf92ae636ed9e8bb48c1541a
NguyenSonArsenal <sonnv.paraline@gmail.com>
2019-07-24 15:43:27 +0700

    update text post5

--- post5.php
+++ post5.php
@@ -23,12 +23,12 @@
                         Và thế là lại nhụt chí, không muốn con mình phải chịu khổ, tâm lý chung mà, ai chả muốn con cái mình sướng...
                         Vậy Bách Khoa là gì, tôi không thể đưa cho họ được một câu trả lời chính xác.
                         Tôi chỉ viết lên đây một vài trải nghiệm cực kì thực tế của bản thân về môi trường học tập ở Bách Khoa, cộng với những kinh nghiệm từ anh/chị cũng đã từng là một phần của Bách Khoa,
-                        vì tối sống rất thực tế và thẳng thắn, có sao nói vậy. Để ai có lỡ đọc được, sẽ có cảm nhận rõ hơn về nơi này.
+                        vì tôi sống rất thực tế và thẳng thắn, có sao nói vậy. Để ai có lỡ đọc được, sẽ có cảm nhận rõ hơn về nơi này.
                     </p>
 
                     <p>
                         <img class="img-reaction" src="<?php assets('assets/images/reactions/emo_flower.gif') ?>" alt="emo_flower.gif">
-                        <strong>Đầu tiên, Bách Khoa, mới nghe thì oai lắm, sang mồm lắm, nhưng thực tế đúng là như vậy.</strong>
+                        <strong>Đầu tiên, Bách Khoa, nghe thì oai lắm, sang mồm lắm, nhưng thực tế đúng là như vậy.</strong>
                         <img class="img-reaction" src="<?php assets('assets/images/reactions/emo_wink.gif') ?>" alt="emo_wink.gif">
                         <br>
                         Bách Khoa trong tôi, là những dãy nhà mọc nối nhau, sừng sững giữa trung tâm thành phố Hà Nội.
@@ -51,7 +51,7 @@
                         Thư viện mới sáng sớm mà đã trật kín người, người ta thấy sinh viên học ở khắp mọi nơi, từ ghế đá tới hồ Tiền, từ gốc cây tới hành lang.
                         Đâu đó, một góc trường, thoáng bạn sinh viên cao gầy, ăn vột xuất cơm trưa để nghiền nát cuốn toán cao cấp, lý đại cương, nguyên lý a, chủ nghĩa b... dày cộp.
                         Có những lúc hỗn độn như vậy mới quý trọng sức lao động của bản thân. Lúc biết điểm, cảm giác sung sướng nhất. Đó không phải là những con điểm 9 hay 10 như cấp 3 nữa.
-                        Chỉ là 4 điểm thôi đã là một sự xuất sắc, cũng đã là một sự cố gắng, đáng để thưởng cho bản thân mình một ly sinh tố dâu size L rồi.
+                        Chỉ là 4 điểm thôi đã là một sự xuất sắc, cũng đã là một sự cố gắng, đáng để thưởng cho bản thân một ly sinh tố dâu size L rồi.
                         <img class="img-reaction" src="<?php assets('assets/images/reactions/emo_love.gif') ?>" alt="emo_love.gif">
                         <br>
                         Cứ qua mỗi kì thi, là danh sách lớp lại vơi đi thấy rõ, hội bạn chơi với nhau cũng bớt đi,
@@ -63,8 +63,8 @@
                         <img class="img-reaction" src="<?php assets('assets/images/reactions/emo_flower.gif') ?>" alt="emo_flower.gif">
                         <strong>Bách khoa trong tôi cũng crush một người, mà không dám ngỏ lời. </strong><br>
                         Nghĩ lại, với bộ não của tôi bây giờ, ngày đó, sẽ không có cơ hội nào cho bạn ý thoát khỏi tay tôi đâu.
-                        Hai đứa quen nhau trong một buổi học cải thiện toán rời rạc, trời hè hà nội nắng nóng như đổ lửa, chợt bạn có hỏi bài tôi. <strong>Xong,</strong> duyên bắt đầu từ đó, không sắp đặt, cũng chẳng hẹn thề trước gì.
-                        Chúng tôi nói với nhau đủ thứ chuyện trên trờ , dưới biển, và tôi có tình cảm với bạn từ khi nào cũng không hay.
+                        Hai đứa quen nhau trong một buổi học cải thiện toán rời rạc, trời hè Hà Nội nắng nóng như đổ lửa, chợt bạn có hỏi bài tôi. <strong>Xong,</strong> duyên bắt đầu từ đó, không sắp đặt, cũng chẳng hẹn thề trước gì.
+                        Chúng tôi nói với nhau đủ thứ chuyện trên trời , dưới biển, và tôi có tình cảm với bạn từ khi nào cũng không hay.
                         Tôi biết bạn thích tôi, ánh mắt bạn nhìn về phía tôi, và nụ cười bạn là dành cho tôi. Tôi cũng crush bạn ý. Nhưng cuộc tình đơn phương của cả 2, dường như không bao giờ có điểm giao nhau.
                         Để giờ đi làm rồi, 25 tuổi, nhìn lại năm tháng đại học, thì ra cô gái đó lại là thứ mà tôi hoài niệm nhất thanh xuân.
                     </p>
@@ -73,7 +73,7 @@
 
                     <p>
                         <img class="img-reaction" src="<?php assets('assets/images/reactions/emo_flower.gif') ?>" alt="emo_flower.gif">
-                        <strong>Vậy nên, </strong>Nếu có một cơ hội, đừng ngần ngại lựa chọn môi trường Bách Khoa. <br>
+                        <strong>Vậy nên, </strong>nếu có một cơ hội, đừng ngần ngại lựa chọn môi trường Bách Khoa. <br>
                         Nơi đó, có thể không cho bạn nhiều thứ sa hoa, bóng lộng, hay những lời đường mật, ong bướm mà ai cũng thích nghe, <strong>nghe cái đó chỉ hại nhau thôi.</strong>.
                         Nhưng tôi chắc chắn, nơi đó, bạn sẽ học được rất nhiều điều, cái quan trọng nhất là bạn bắt buộc, không còn cách nào khác là tự đi trên đôi chân của mình, <strong>không dựa dẫm vào bất kỳ bố con thằng nào.</strong>
                         Nơi đó cũng sẽ chứng kiến rất nhiều sự trưởng thành trong con người bạn, cho bạn những hành trang, mai này <strong>đủ sức chống trọi với những đợt sóng gió đánh vỗ mặt của cuộc đời.</strong>
