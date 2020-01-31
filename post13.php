094ef686cefe21b4ddf470795bddc05c397d081f
NguyenSonArsenal <vanson297.nguyen@gmail.com>
2020-01-31 21:54:36 +0700

    + add post

---
+++ post13.php
@@ -0,0 +1,266 @@
+<?php
+$metaTitle = "Tổng hết 2020, và kế hoạch 2021";
+$metaDescription = "";
+$metaKeyword = "";
+$canonical = "http://sonnguyen-vlog.top/";
+
+include './includes/header.php';
+?>
+
+<!-- Content: Post detail-->
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="content__heading">
+                    <div class="content__title">
+                        <h1 class="color-red">Tổng kết 2020 và kế hoạch 2021. </h1>
+                    </div>
+                    <div class="content__time"><span>Ngày 31 tháng 1 năm 2020</span></div>
+                    <div class="content__time"><small>(Bài viết số 0)</small></div>
+                </div>
+
+                <div class="content_body text-justify">
+                    <p>
+                        <strong class="color-red">Lần này là rút kinh nghiệm luôn, lần sau cứ thế mà thực hiện. Là những kinh nghiệm sương máu với đời,
+                            để đàn e học cái hay, né cái dở. </strong> <br>
+                        <br>
+                        Nội dung gồm 2 phần lớn: <br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1. Tổng kết lại tất tần tật mọi thứ về gia đình, các khoản vay, nợ, chuẩn bị trước, trong và sau Tết<br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2. Lên kế hoạch cụ thể, chi tiết, mục tiêu cho năm 2021
+                    </p>
+
+                    <p>
+                        <strong class="color-red">I. Tổng kết năm 2020</strong><br>
+
+                        <strong>1. Trả nợ: (40m) Đã hoàn thành</strong><br>
+                            <ul>
+                                <li>Ngân hàng: 20m</li>
+                                <li>Lãi ngân hàng tháng 1: 1.5m</li>
+                                <li>Nợ bác Luân: 2.5m</li>
+                                <li>Bố nợ tiền bột: 6m</li>
+                                <li>Mua tivi: 10m</li>
+                            </ul>
+
+                        <strong>2. Nợ mới (9m)</strong><br>
+                            <ul>
+                                <li>A Hoàng Min (cty): 5m</li>
+                                <li>
+                                    A Hà (nhà bác phương - nợ mua tivi): 3.5m <br>
+                                    &nbsp;&nbsp;&nbsp;Deadline: 5/3/2020
+                                </li>
+                                <li>
+                                    Bác Năm: 500k <br>
+                                    &nbsp;&nbsp;&nbsp;Deadline: 5/3/2020
+                                </li>
+                            </ul>
+
+                        <strong>3. Những người còn nợ mình</strong><br>
+                            <ul>
+                                <li>Thương bao: 1.4m <br> &nbsp;&nbsp;&nbsp; Deadline: Đầu tháng 3 </li>
+                                <li>Sơn quế: 2m <br> &nbsp;&nbsp;&nbsp; Deadline: Không có deadline </li>
+                                <li>E Thủy hàng xóm trọ: 2m <br> &nbsp;&nbsp;&nbsp; Deadline: 8/2/2020 </li>
+                            </ul>
+
+                    <strong>4. Tết thì chuẩn bị những gì (Sắp xếp theo thứ tự ưu tiên)</strong><br>
+                    - 2 thùng bia chai Hà Nội: Mua vào tầm 23 tết. <br>
+                    &emsp;&emsp; 2 thùng đã thừa oy, k nhất thiết phải 3 thùng.<br>
+                    &emsp;&emsp; K mua bia lon (vì đắt hơn, mà uống lãng phí)<br>
+                    - 1 thùng bò húc <br>
+                    - 1 thùng nước ngọt (nutri or 7 up, coca...) <br>
+                    - 1 kg hướng dương. (Mua trước 25 tết, vì món này hết nhanh, với mua sớm, hàng ngon) <br>
+                    &emsp;&emsp; + Ưu tiên loại sấy sẵn oy, mua về sẵn ăn, k phải rang. <br>
+                    - Có điều kiện thì mua 2 thùng thạch sữa chua long hải (loại đóng thùng, 5-6kg/150k-165k/thùng) (Ăn đẹp da)
+                    <br>
+                    &emsp;&emsp; Cái này để được lâu, mua càng sớm càng tốt, (thậm chí mua từ 15 tết) làm quà đi biếu cũng hay, tình cảm.
+                    <br>
+                    - 1 kg kẹo thập cẩm (Loại kẹo dẻo, k mua kẹo cứng) (cái này chiều 30 tết xuống vạn, còn đầy) <br>
+                    - Lên đồ trên bàn thờ (bàn mẹ, và bàn gia tiên) (dưới đây là áp dụng cho bàn mẹ, bàn gia tiên tương tự)
+                    <br>
+                    &emsp;&emsp;+ Tất cả các đồ đặt lên bàn thờ đều phải lẻ <br>
+                    &emsp;&emsp;+ Mâm ngũ quả <br>
+                    &emsp;&emsp;&emsp;&emsp; Chuối (Bắt buộc) (27 28 tết mua đi, thậm chí 29 mua cũng được)(loại chuối xanh, quả to, nhiều quả)
+                    <br>
+                    &emsp;&emsp;&emsp;&emsp; Bưởi (Bắt buộc): Mua phù hợp với nải chuối, nải chuối to, mua quả to <br>
+                    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Bắt buộc chọn quả còn cả cuống, lá xanh (Vì nhiều lộc lá) <br>
+                    &emsp;&emsp;&emsp;&emsp; Ngoài ra còn thêm: Quýt (mua theo từng chùm), ớt, táo, lê, cam, các quả màu đỏ, vàng. <br>
+                    &emsp;&emsp;&emsp;&emsp; Lưu ý: K mua dứa gai (vì gai góc, chênh vênh) <br>
+                    &emsp;&emsp;&emsp;&emsp; Quan trọng: Lúc lên đồ ở bàn thờ, hãy đặt nản chuối hướng ra ngoài ánh mắt của người nhìn vào,
+                    k để lấp đằng sau. <br>
+                    &emsp;&emsp; + Hương, vàng: Chọn loại hương cuộn (cuộn sẽ cho nhiều lộc lá), k mua loại hương đối đến đâu, thì tàn đến đó <br>
+                    &emsp;&emsp; + Bánh trưng: <br>
+                    &emsp;&emsp;&emsp;&emsp;Nhờ bác Phương gói: tối đa 3 cái thôi. 1 cái ăn mùng 1 tết,
+                    2 cái đặt lên bàn thờ mẹ vs gia tiên. <br>
+                    &emsp;&emsp;&emsp;&emsp;Nếu không, đi mua. Cũng chỉ cần 3 cái mà thôi. <br>
+                    &emsp;&emsp;+ 3-5 gói bánh. Mua loại thật ngon. Tối đa cũng chỉ có 50k/gói. <br>
+                    &emsp;&emsp;&emsp;&emsp; Bắt buộc phải có 1 gói chocopie Tình loại 12 cái, tầm 50k. Các loại chocopie khác, k mua. <br>
+                    &emsp;&emsp; + 1 đĩa hoa quả  <br>
+                    &emsp;&emsp; + Hoa: 3-5 bông, cho 1 lọ hoa  <br>
+                    &emsp;&emsp; + Dư tài chính: mua 1 giỏ quà đẹp, tầm 200-300k/giỏ  <br>
+                    - Kể từ chiều 30 tết đến hết mùng 3 tết: <br>
+                    &emsp;&emsp;Cứ tới bữa ăn, là thắp hương cho mẹ, nấu cơm mời mẹ, trên bàn mẹ, lúc nào cũng phải có cơm, canh. <br>
+                    - Chiều 30 tết, ra mộ mẹ, thắp hương, mời mẹ về ăn tết. <br>
+                    - Đi tết nhà bà, bác Luân: Mua giỏ quà đi cho sang, tầm 200-250k/giỏ <br>
+                    - Mua quần áo cho tụi nhỏ: Tối đa 1m <br>
+                    &emsp;&emsp; Thằng cu nhà o Ngọc <br>
+                    &emsp;&emsp; 2 đứa nhà bà <br>
+                    &emsp;&emsp; Mua: Quần, áo, giầy, mũ, thắt lưng, túi, tất (Tùy kinh tết mà full or thêm, or cắt giảm). <br>
+                    - Đồ dự trù bỏ tủ lạnh <br>
+                    &emsp;&emsp; Các loại rau: Đủ sài trong 3 ngày, trường hợp có khách vip đến chơi, cũng xoay được, k phải đi mua <br>
+                    &emsp;&emsp; Miến: 5 lạng <br>
+                    &emsp;&emsp; Làm thịt sẵn 3 con gà (nếu có, k thì 2 con) <br>
+                    &emsp;&emsp; Thịt lợn đỏ: 1 kg <br>
+                    - Gia vị: muối bột canh, dầu ăn, bột nêm, mì chính, chanh, tỏi, hành đầy đủ. <br>
+                    - Bát, đũa, cốc, chén. <br>
+                    - Đào, quất, ... Có cũng được, không có cũng chẳng chết ai. <br>
+
+                    ===> Tất tần tật những việc này, cái gì làm trước, làm luôn. Còn lại, tối đa làm trong vòng 1 ngày 29 tết. Ngày 30 tết, free, đi chơi. <br>
+                    <br>
+                    <strong>5. Đi vào chi tiết chi tiêu tết 2020 vừa qua: Tối đa chi 4m</strong> <br>
+                    <ul>
+                        <li> Lì xì công ty: 550k </li>
+                        <li>Lì xì bà: 200</li>
+                        <li>Lì xì khác: 500</li>
+                        <li>Bia: 3 thùng: 135x3 = 400</li>
+                        <li>Giỏ quà: 3: 800k</li>
+                        <li>Bánh ngọt: 300k</li>
+                        <li>Kẹo thập cẩm: 1kg: 100k </li>
+                        <li>Bò húc: 1 thùng: 250</li>
+                        <li>Nước ngọt: Nutri: 1 thùng: 200k</li>
+                        <li>Rau, củ, quả, thịt, xăng xe, chùa... 500k</li>
+                        <li>Mua quần áo cho tụi nhỏ: Con o Ngọc, 2 đứa nhà chú Thành: 1,2m </li>
+
+                        <br>
+
+                        <img class="img-reaction" src="<?php assets('assets/images/reactions/hack/icon_1.png') ?>">
+                        Tổng: 4450 Con số có thể chấp nhận được. Đó là tết này k tham gia bất kì một cuộc vui nào, và nhà cũng có sẵn gà oy.
+                        Nếu không cũng lên 5m là chắc.
+                    </ul>
+                    </p>
+
+                    <p>
+                        <strong class="color-red">II. Kế hoạch, mục tiêu 2021</strong> <br>
+
+                        <strong>1. Kế hoạch, mục tiêu 2021 </strong> <br>
+
+                        - Lên mục tiêu ngay từ bây giờ, để tiết kiệm đủ 60m trả lãi ngân hàng vào tết 2021. <br>
+                        &emsp;&emsp;Con số 60m, thực sự quá lớn, nếu k chuẩn bị ngay từ bây giờ, thì chỉ có bán thân đi trả nợ thôi.
+                        <br>
+                        &emsp;&emsp;
+                        <img class="img-reaction" src="<?php assets('assets/images/reactions/hack/icon_1.png') ?>">
+                        Mục tiêu, 6 tháng đầu năm, 2 thằng phải tiết kiệm được 20m <br>
+
+                        <br>
+                        - Tổng nợ bác Phương còn lại: 55m, cố gắng trong năm nay trả được 20m. <br>
+                        - Tổng nợ nhà bác Lâm: 30m - 40m <br>
+
+                        <br>
+                        <img class="img-reaction" src="<?php assets('assets/images/reactions/hack/icon_1.png') ?>">
+                        Trong năm nay, không có mua sắm gì hết, tất tần tật $ đầu tư để tìm người iu (người phù hợp nhất) cho cả 2 đứa, và trả nợ.
+                        Đợt mua cái tivi là sai lầm. Bỏ ra đống $ oy để đó. <br>
+
+                        <br>
+                        - Cá nhân: <br>
+                        &emsp;&emsp;<strong>Sơn:</strong> Tìm được người iu mình phù hợp nhất + tiết kiệm $ là ưu tiên số 1   <br>
+                        &emsp;&emsp;&emsp;&emsp;+ Làm: <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Trong tháng 2, phải hoàn thành website bán hàng <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Đầu tư $ để mua host, domain, và go live web bán hàng <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Sẽ đi làm thêm thứ 7 không lương, cho đến khi web bán hàng chính thức go live
+                        <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Tìm hiểu về nguồn hàng. Tìm cách sang Trung quốc, nhập hàng về bán,
+                        coi như là một chuyến đi du lịch. (phải có kế hoạch chi tiết) <br>
+                        &emsp;&emsp;&emsp;&emsp;+ Học: <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Học thêm content để chuyển dần sang viết lách -> Tìm việc làm thêm về viết content
+                        <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Học thêm SEO (3 tháng cuối năm, or có đủ kinh tế ổn thì sẽ học sớm) -> không thì sang năm học
+                        <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Học thêm tiếng a: K ưu tiên <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Xem kẽ là đi học nhảy, học 1-2 tháng lại nghỉ 1-2 tháng để học cái khác <br>
+
+                        &emsp;&emsp;&emsp;&emsp;+ Chơi: <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Chi tiêu có kế hoạch, không thích gì thì mua nữa. <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Gặp người phù hợp, chốt luôn (Bán kính: 50km).
+                        Tỏ tình, nói chuyện người lớn, nghiêm túc luôn. Không dài dòng, quan tâm, nhắn tin nữa. <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 30.4 du lịch công ty <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Team du xuân <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Team du lịch hè <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Teambuilding tham gia đầy đủ <br>
+
+                        &emsp;&emsp;&emsp;&emsp;+ Tiết kiệm: <br>
+
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Bắt đầu từ tháng 6, trong tài khoản lúc nào cũng phải có 20m, ai cần phát là có sài luôn. <br>
+
+                        &emsp;&emsp;<strong>Đức:</strong> <br>
+                        &emsp;&emsp;&emsp;&emsp;+ Làm: 70% <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Trong tháng 2, bắt buộc, bằng mọi cách, phải có công việc làm chính thức fulltime <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; <strong>Quan trọng nhất, ưu tiên cao nhất: Từ giờ tới đầu tháng 4 dương lịch, bắt buộc phải gửi về nhà được 10m.</strong> <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                        <img class="img-reaction" src="<?php assets('assets/images/reactions/hack/icon_1.png') ?>">
+                        Nếu làm được 2 việc trên, thì phần còn lại của năm sẽ dễ dàng hơn với 2 ae <br>
+                        &emsp;&emsp;&emsp;&emsp;+ Học: 10% <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Song song với làm, thì hoàn thành topik3, và giao tiếp cơ bản tiếng Hàn.
+                        <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Tức nói cho bọn Hàn hiểu và hiểu bọn Hàn nói. <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Ngoại ngữ là vốn vô giá, về Việt Nam làm cho công ty Hàn or cty về tiếng Hàn.
+                        <br>
+                        &emsp;&emsp;&emsp;&emsp;+ Chơi: 20% <br>
+                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Tìm người iu phù hợp nhất: Đúng người, chốt luôn, không dài dòng
+                            <i>(Phải hỏi ý kiến của anh với bố)</i> <br>
+
+                        <br><strong>2. Kinh nghiệm sương máu </strong> <br>
+                        Dưới đây là một vài kinh nghiệm sương máu, a muốn chia sẻ với đàn e. Hễ gặp thì còn biết mà ứng phó.
+                        <br>
+                        - Tiền là liều thuốc thử nặng đô nhất với một con người.
+                        Vì vậy, muốn biết bộ mặt thật của 1 người, hãy nói chuyện về $ với họ, cho họ vay tiền, or vay $ họ.
+                        <br>
+                        - Nếu mới quen một người con gái, mà nó chỉ nhắc tới $, thì con đó chỉ để chịch thôi, không lấy làm vợ được.
+                        <br>
+                        - Tiền vào tết bị mất giá, luôn luôn phải giữ cái đầu lạnh, $ rất khó kiếm, không thể phá, sài vô tội vạ được.
+                        <br>
+                        - Không thể thích cái gì là mua, phải mua theo nhu cầu. <br>
+                        - <strong>Cực kì quan trọng</strong>: Lên kế hoạch rất chi tiết, càng chi tiết càng tốt cho tất cả mọi việc trên đời này. <br>
+                        Tiêu gì, mua gì. Đi chơi đâu, vào giờ nào, ngày nào. Chỗ nào phải đi, chỗ nào không đáng đi thì bỏ luôn.
+                        <br>
+                        - Đặt ra con số tối đa chi cho mọi cuộc chơi. Ví dụ tết 2021, chi tối đa 4m cho tất tần tật: ăn chơi, mua sắm, đi tết, lì xì...
+                        Có thế thôi, hết oy ở nhà ngủ. <br>
+                        - Bắt buộc: Phải có tiền tiết kiệm trước tết (con số ở đây là 6m - đủ sống 1 tháng sau tết),
+                        vì vào tết tiêu hoang, nếu không tiết kiệm từ trước tết, thì ăn tết xong, chết đói là cái chắc. <br>
+                        - Lúc cho ai vay $, nói rõ ràng luôn, cho vay bao nhiêu, ngày nào trả được. Không được ngại. <br>
+                        &emsp;&emsp;Nếu là bạn bè bình thường, tối đa cho vay 3m trong vòng 3 tháng. <br>
+                        &emsp;&emsp;Bạn thân, đi làm cùng công ty, bạn cùng phòng, hiểu nhau, đủ tin tưởng... tối đa 5m trong vòng 5 tháng.
+                        <br>
+                        &emsp;&emsp;Nói rõ luôn, phải trả đúng hạn, cho vay 3 tháng thì 2 tháng phải trả được một nửa, cho vay 5m thì 3 tháng phải trả được 1 nửa.
+                        <br> &emsp;&emsp;Tức là cho vay n tháng thì n/2 tháng phải trả được 1 nửa. <br>
+                        &emsp;&emsp;Hoặc chơi chắc, mỗi tháng mày phải trả tao 1 số $ nào đó. $ là thứ không thể mang ra làm trò đùa được, mình cũng cần $ để đầu tư nữa.
+                        <br>
+                        &emsp;&emsp; <strong>Những người trễ deadline khi mình cho vay $, hoặc lươn lẹo, sẽ KHÔNG BAO GIỜ có lần thứ 2 cho vay nữa.
+                            Không cần biết lý do. Cho next luôn. </strong><br>
+                        - Dùng nhiều thời gian để <strong>QUAN SÁT</strong> một con người, mọi thứ xung quanh thay đổi theo thời gian.
+                        <br>
+                    </p>
+
+                    <br><strong>3. Tâm sự</strong> <br>
+                    Tất cả 2 anh em: Kiếm $, tìm người iu. Nhưng không phải là làm mọi cách để kiếm $, vồ vập với người iu.
+                    <br>
+                    Cái gì cũng phải có thời gian và quá trình. Không thể một phát ăn ngay được. <br>
+                    <strong>Sức khỏe, tính mạng con người là trên hết</strong>.
+                    Nếu công việc nguy hiểm, như trèo cao, đu dây, làm quá giờ, quá khuya... thì tuyệt đối không làm. Đặt con người lên trên hết.
+                    <br>
+                    Tất cả đi ngủ trước 11h đêm. <br>
+
+                    <br><strong class="color-red">III. Kết luận</strong> <br>
+                    <small>End game tại đây thôi. Kế hoạch đã lên, hãy thực hiện nó một cách nghiêm túc.
+                            Để sau 1 năm nhìn lại, 2 đứa sẽ có tất cả mọi thứ. Good luck tới cả 2</small>
+
+                    <div class="action">
+                        <a class="color-red" href="ngay-cuoi-thap-ki">
+                            <small><< Ngày cuối thập kỉ .</small>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include './includes/footer.php'; ?>
+
